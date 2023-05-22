155edc009e0cc17406828db5fce45e6e3e52c750
varad-magdum <varadmagdum33@gmail.com>
2023-05-22 22:46:52 +0530

    done

---
+++ dom.js
@@ -0,0 +1,33 @@
+// // Create a button , on click of which new Heading tag h1 should be added with text as "MERN stack" on the screen above button
+// document.getElementById("btn").addEventListener("click", function () {
+//     document.getElementById("h1").innerHTML="mern stack"
+// })
+// Que3) Write code to get 1st H1 element from a webpage using DOM
+// document.getElementsByTagName("h1")[0].innerHTML="varad"
+// function clock(){
+//      var a = new Date().toLocaleTimeString();
+//       document.getElementById("sample").innerText=a;
+//     }
+//     setInterval(clock,1000);
+// document.getElementById("btn").addEventListener("click", function(){
+//     document.getElementById("h1").innerHTML= "welcome to elevation academy"
+// })
+
+    
+// function timer() {
+//     let date = new Date();
+// let hr = document.getElementById("hr");
+// let min = document.getElementById("min");
+// let sec = document.getElementById("sec");
+// let hour = date.getHours();
+// let minutes = date.getMinutes();
+// let seconds = date.getSeconds();
+// hr.innerHTML = (hour);
+// min.innerHTML = (minutes);
+// sec.innerHTML = (seconds);
+
+// }
+// setInterval(timer, 1000);
+let arr = [1, 2, 3, 4.5];
+arr.reduce
+
---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click", function () {
+    document.getElementById("varad").innerHTML = "mern stack"
+})
+arr.map
